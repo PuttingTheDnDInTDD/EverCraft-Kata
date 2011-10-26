450422386bf1c739928ac0b3dc31a629a85b99a9
Guy Royse <guy@guyroyse.com>
2011-10-26 14:19:25 -0400

    Added Armor class for Character

--- ruby/lib/evercraft.rb
+++ ruby/lib/evercraft.rb
@@ -1,2 +1,7 @@
 class Character
+  attr_accessor :name
+  
+  def armor_class
+    10
+  end
 end
--- ruby/spec/evercraft_spec.rb
+++ ruby/spec/evercraft_spec.rb
@@ -1,4 +1,11 @@
 require 'evercraft'
 
 describe Character do
+  it "has a name" do
+    subject.name = 'Aragon'
+    subject.name.should == 'Aragon'
+  end
+  it "had a default armor class of 10" do
+    subject.armor_class.should == 10
+  end
 end
