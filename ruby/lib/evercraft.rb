879fc3141caf3140593b30a2c09755a5c04549f5
Guy Royse <guy@guyroyse.com>
2011-10-26 15:55:49 -0400

    almost finished first iteration

--- ruby/lib/evercraft.rb
+++ ruby/lib/evercraft.rb
@@ -1,7 +1,39 @@
 class Character
-  attr_accessor :name
   
-  def armor_class
-    10
+  attr_accessor :name, :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma
+  attr_reader :hit_points, :armor_class
+  
+  def initialize
+    @hit_points = 5
+    @armor_class = 10
+    @strength = Ability.new
+    @dexterity = Ability.new
+    @constitution = Ability.new
+    @intelligence = Ability.new
+    @wisdom = Ability.new
+    @charisma = Ability.new
   end
+
+  def attack(target, roll)
+    !target.defend roll
+  end
+
+  def defend(roll)
+    hit = roll >= @armor_class
+    damage = (roll == 20) ? 2 : 1
+    @hit_points -= damage if hit
+    !hit
+  end
+
+end
+
+class Ability
+
+  attr_reader :score, :modifier
+
+  def score=(score)
+    @score = score
+    @modifier = (@score - 10) / 2
+  end
+
 end
